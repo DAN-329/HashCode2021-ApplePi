c7c1b406246441e4695813e82d3b10c95fc2ed1b
Alex Michael Manjakunnel <amanjakunnel@MacBook-Air.local>
2022-02-13 07:09:26 +0530

    final commit

--- Flask/mindmapgen.py
+++ Flask/mindmapgen.py
@@ -1,30 +1,20 @@
 import os
 import docx
-import PyPDF2
 import spacy
 import RAKE
-import operator
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import PDFPageAggregator
+from pdfminer3.converter import TextConverter
+import io
 from collections import Counter
 from string import punctuation
-'''import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download  ('omw-1.4')'''
 from nltk.corpus import stopwords, wordnet
 from nltk import pos_tag
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from rake_nltk import Rake
 
 def extract(filepath):
     file_ext = os.path.splitext(filepath)[1]
@@ -35,14 +25,16 @@ def extract(filepath):
             fullText.append(para.text)
         return '\n'.join(fullText)
     if file_ext == '.pdf':
-        pdfFileObj = open(filepath, 'rb')
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-        n = pdfReader.numPages
-        text = ''
-        for i in range(n):
-            pageObj = pdfReader.getPage(i)
-            text += pageObj.extractText()
-        pdfFileObj.close()
+        resource_manager = PDFResourceManager()
+        fake_file_handle = io.StringIO()
+        converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+        page_interpreter = PDFPageInterpreter(resource_manager, converter)
+        with open(filepath, 'rb') as fh:
+            for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+                page_interpreter.process_page(page)
+            text = fake_file_handle.getvalue()
+        converter.close()
+        fake_file_handle.close()
         return text
     if file_ext == '.txt':
         myfile = open(filepath, "rt")
@@ -80,25 +72,6 @@ def get_wordnet_pos(word):
     tag_dict = {"J": wordnet.ADJ, "N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV}
     return tag_dict.get(tag, wordnet.NOUN)
 
-
-def penn2morphy(penntag):
-    morphy_tag = {'NN':'n', 'JJ':'a', 'VB':'v', 'RB':'r'}
-    try:
-        return morphy_tag[penntag[:2]]
-    except:
-        return 'n' 
-
-def spacy_extraction(text):
-    nlp = spacy.load("en_core_web_lg")
-    doc = nlp(text)
-    return doc.ents
-
-def rake_nltk_extraction(text):
-    rake_nltk_var = Rake()
-    rake_nltk_var.extract_keywords_from_text(text)
-    keyword_extracted = rake_nltk_var.get_ranked_phrases()
-    return keyword_extracted
-
 def rake_extraction(text):
     stop_dir = 'StopWords.txt'
     rake_object = RAKE.Rake(stop_dir)
@@ -110,7 +83,7 @@ def Sort_Tuple(tup):
     tup.sort(key = lambda x: x[1])
     return tup
 
-def spacy_extraction_alt(text):
+def spacy_extraction(text):
     nlp = spacy.load("en_core_web_lg")
     result = []
     pos_tag = ['PROPN', 'ADJ', 'NOUN']
@@ -122,37 +95,6 @@ def spacy_extraction_alt(text):
             result.append(token.text)
     output = [(x[0]) for x in Counter(result).most_common(5)]
     return output
-
-def text_summarization(text):
-    stopWords = set(stopwords.words("english"))
-    words = word_tokenize(text)
-    freqTable = dict()
-    for word in words:
-        word = word.lower()
-        if word in stopWords:
-            continue
-        if word in freqTable:
-            freqTable[word] += 1
-        else:
-            freqTable[word] = 1
-    sentences = sent_tokenize(text)
-    sentenceValue = dict()
-    for sentence in sentences:
-        for word, freq in freqTable.items():
-            if word in sentence.lower():
-                if sentence in sentenceValue:
-                    sentenceValue[sentence] += freq
-                else:
-                    sentenceValue[sentence] = freq
-    sumValues = 0
-    for sentence in sentenceValue:
-        sumValues += sentenceValue[sentence]
-    average = int(sumValues / len(sentenceValue))
-    summary = ""
-    for sentence in sentences:
-        if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
-            summary += " " + sentence
-    return summary
 
 def keyphrase_extraction(text, keywords):
     d = {}
@@ -189,42 +131,9 @@ def nodes(title, d):
             id += 1
     return l
 
-#print(prenlp(paras(extract("smash.docx"))))
-title = extract("smash.docx").split('\n')[0]
-text = prenlp(paras(extract("smash.docx")))
-keywords = spacy_extraction_alt(text)
+doc = extract("smash.pdf")
+title = doc.split('\n')[0]
+text = prenlp(paras(doc))
+keywords = spacy_extraction(text)
 phrases = keyphrase_extraction(text, keywords)
 print(nodes(title, phrases))
-
-
-'''
-print(spacy_extraction_alt(prenlp(paras(extract("smash.docx")))))
-
-print(prenlp(extract("mitochondria.txt")))
-
-text = extract("mitochondria.txt")
-print(spacy_extraction_alt(text))
-
-text = extract("mitochondria.txt")
-title = text.split('\n', 1)[0]
-ll = [{"id": 1, "label": "Interests"}]
-curr = 1
-d = {}
-p = paras(text)
-prep = prenlp(text)
-summary = text_summarization(p)
-keywords = spacy_extraction(summary)
-p = p.replace('.\n\n', '. ')
-p = p.replace('.\n', '. ')
-sentences = p.split('. ')
-for sent in sentences:
-    for key in keywords[:len(keywords)//3]:
-        curr += 1
-        if key not in d:
-            d[key] = curr
-            ll.append({"id": curr, "label": key, "parent": 1})
-        if key in sent:
-            curr += 1
-            ll.append({"id": curr, "label": sent, "parent": d[key]})
-
-print(ll)'''
